3b7bdc5645fb802173e5273d27de6a0c68d5f6a7
Roger Ehmpke <rehmpke@gmail.com>
2018-12-06 23:51:18 -0600

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,8 +7,10 @@ import Image from '../components/image'
 const IndexPage = () => (
   <Layout>
     <h1>Front End Developer</h1>
-    <p>I enjoy building and collaborating with others using the JAMStack.</p>
-    <p>Currently I am working with Gatsby, React, and WebPack.</p>
+    <p>I am a web coordinator currently focusing on the field of frontend web development and JavaScript programming, React, and Redux. My current goal is to obtain either remote web development opportunities and/or part-time/contract employment as a frontend web application developer.</p>
+    <p>For over 2 decades I have designed and built portal sites to address design problems. I manage sites in the education industry and have in the nutritional supplement industry as well. I strive to create usable and polished products through passionate and deliberate design.</p>
+    <p>I live in Kankakee Illinois. I enjoy music, oil painting, and single malt whiskey.</p>
+    <p>Contact me or reach out via LinkedIn or send an email.</p>
     <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
       <Image />
     </div>
